0f527cad1ca4ba33b48bf7b2f941e48f8e14d90d
Lisa Bevin <lisabevin@gmail.com>
2020-01-20 11:49:56 -0400

    Updates

---
+++ precache-manifest.4d46da7de02868828f72ac984e344955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f49ce188c65f75353548c83248cfde2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "699e50f496fd5df26117",
+    "url": "/monsters-rolodex/static/css/main.5a32f192.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "699e50f496fd5df26117",
+    "url": "/monsters-rolodex/static/js/main.59b74170.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
